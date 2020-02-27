8182e8d895816af4498be1e50ace97fc1aff4326
Fabián Alejandro Mondragón Erazo - Ceiba Software <fabian.mondragon@ceiba.com.co>
2020-02-27 16:50:36 -0500

    add: finalize with RecyclerView

--- app/src/main/java/com/example/todo/ui/MainActivity.kt
+++ app/src/main/java/com/example/todo/ui/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.core.entity.Task
 import com.example.todo.R
 import com.example.todo.framework.TodoViewModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,11 +23,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         btnAddTask = findViewById(R.id.bntAddTask)
         viewModel = ViewModelProviders.of(this).get(TodoViewModel::class.java)
         bntAddTask.setOnClickListener(this)
+        recyclerView.layoutManager = LinearLayoutManager (this)
+        recyclerView.adapter = TaskAdapter (ArrayList<Task>(),this)
     }
 
     override fun onClick(v: View?) {
-
         viewModel.registerTask()
-
     }
 }
---
+++ app/src/main/java/com/example/todo/ui/TaskAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.todo.ui
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.core.entity.Task
+import com.example.todo.R
+import kotlinx.android.synthetic.main.item_task.view.*
+
+class TaskAdapter (private val items : ArrayList<Task>, private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_task, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder?.textViewItemDescription?.text = items[position].description
+        holder?.textViewItemtTitle?.text = items[position].name
+    }
+
+    override fun getItemCount(): Int = items.size
+
+}
+
+class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+    // Holds the TextView that will add each animal to
+    val textViewItemtTitle = view.textViewItemTitle
+    val textViewItemDescription = view.textViewItemDescription
+
+}
